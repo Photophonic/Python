72cbe03e465db954c48f36974f845fb4a1ec760f
Seth Martin <sgm13555@gmail.com>
2023-07-04 20:06:15 -0500

    lesson 01

---
+++ Lesson01/hello.py
@@ -0,0 +1,13 @@
+#this program does stuff
+
+print('stuff')
+print('what is your name???')
+myName = input()
+print('It \'s good to meet you ' + myName)
+print('The lenght of your name is: ')
+print(len(myName))
+print('What is your age?') #ask their name
+myAge = input()
+print('you will be ' + str(int(myAge)+1) + ' in a year.')
+
+      
