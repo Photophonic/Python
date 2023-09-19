10d3476811f6c9f9bc9a8ff47cf845c419b5a7bd
Seth Martin <sgm13555@gmail.com>
2023-09-18 19:46:36 -0500

    Classes

--- ch8/making_pizza.py
+++
@@ -1,5 +0,0 @@
-import pizza
-
-# import NAME allows python to call a separate function and used it in this file
-# using the imported function is similar to pl/sql, file.function_name
-pizza.make_pizza('Medium', 'Cheese', 'Honey Wheat Crust', 'Sausage')
