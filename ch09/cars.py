8e18b17013609ff253ed2426ca273792685969ef
Seth Martin <sgm13555@gmail.com>
2023-09-24 20:15:46 -0500

    Classes

--- ch09/cars.py
+++ ch09/cars.py
@@ -19,24 +19,49 @@ class Car:
         if (mileage >= self.odometer):
             self.odometer = mileage
 
+    def increment_odometer(self, mileage):
+        self.odometer += mileage
+
     def read_odometer(self):
         """read the odometer"""
         print(f'{self.model} has {self.odometer} miles on it.')
 
 
-my_car = Car('toyota', '4Runner', 2021)
-kelly_car = Car('mini', 'countryman', 2024)
-print(my_car.get_description())
-print(kelly_car.get_description())
+class ElectricCar(Car):
+    """class for an electric car, inheriting the Car class"""
 
-# access the attribute directly and update it in MY instance of the class
-# my_car.odometer = 40000
+    # Note, used double _ not triple _
+    def __init__(self, make, model, year):
+        """define items specific to the Electric car"""
+        # super() allows you to call methods from the parent class
+        super().__init__(make, model, year)
 
-# The better way to do this is through a method in the class
-my_car.update_odometer(40000)
+        # now that a child class is connected, you can add your own
+        # special attributes unique to this class
+        self.range = 400
+        self.drive = '2WD'
 
-my_car.read_odometer()
+    def get_range(self):
+        print(f'\nThe {self.make} {self.model} has a range of {self.drive}')
 
-my_car.update_odometer(50000)
 
-my_car.read_odometer()
+my_Leaf = ElectricCar('Toyota', 'Rav4 Prime', '2024')
+my_Leaf.get_range()
+
+
+# my_car = Car('toyota', '4Runner', 2021)
+# kelly_car = Car('mini', 'countryman', 2024)
+# print(my_car.get_description())
+# print(kelly_car.get_description())
+
+# # access the attribute directly and update it in MY instance of the class
+# # my_car.odometer = 40000
+
+# # The better way to do this is through a method in the class
+# my_car.update_odometer(40000)
+
+# my_car.read_odometer()
+
+# my_car.update_odometer(50000)
+
+# my_car.read_odometer()
--- ch09/restaurant.py
+++ ch09/restaurant.py
@@ -4,12 +4,25 @@ class Restaurant:
     def __init__(self, name, style):
         self.name = name
         self.style = style
+        self.served = 0
+        self.span = ''
 
     def describe_place(self):
         print(f'{self.name} specializes in {self.style}')
 
     def open_busingess(self):
         print(f'{self.name} is now open for the day')
+
+    def update_served(self, customers):
+        self.served = customers
+
+    def increment_served(self, span, customers):
+        self.served += customers
+        print(f'\nThere were {customers} in the last {self.span}')
+        print(f'\nThis brings the total served to {self.served}')
+
+    def number_served(self):
+        print(f'The number of customers served is {self.served}')
 
 
 local_restaurant = Restaurant('Long Boards', 'Hawaiian style wraps')
@@ -21,3 +34,11 @@ local_burger = Restaurant('Snack Shack', 'Burgers and Shakes')
 local_restaurant.describe_place()
 local_pizza.describe_place()
 local_burger.describe_place()
+
+local_restaurant.number_served()
+local_restaurant.update_served(50)
+local_restaurant.number_served()
+
+
+local_restaurant.increment_served('Day', 100)
+local_restaurant.increment_served('Week', 1500)
