982a7036eeea683695f93b71a45d81085bae5b18
Justin Bailey <sgm13555@gmail.com>
2024-06-03 20:25:52 -0500

    Bank program

--- TutorialForBeginners/ch20/bank_acounts.py
+++ TutorialForBeginners/ch20/bank_acounts.py
@@ -53,3 +53,43 @@ class BankAccount:
             print("\n*********\n")
         except BalanceException as error:
             print(f"\nWithdraw interupted: {error}")
+
+
+# create a new class with inheritance from the original calss
+class IntrestRewards(BankAccount):
+    # pass in the parent we are inheriting from but no new properties.
+    # def __init__(self, initialAmount, acctName):
+    #     super().__init__(initialAmount, acctName)
+
+    # this will override the deposit amount
+    def deposit(self, depositAmount):
+        self.balance = self.balance + (depositAmount * 1.05)
+        print("Deposit compete.")
+
+        # call balance
+        self.getBalance()
+
+
+class SavingsAccount(IntrestRewards):
+    # This will add a new property
+    def __init__(self, initialAmount, acctName):
+        super().__init__(initialAmount, acctName)
+
+        # add a new property onto the new class
+        # this will subtract $5 anytime there is a withdraw
+        self.fee = 5
+
+    # override the withdraw method
+    def withdrawAmount(self, withdrawAmount):
+        # use of the try/except to test for possible error due to funds available
+        try:
+            # check if there is enough to complete the transaction
+            self.balanceCheck(withdrawAmount)
+            self.balance -= withdrawAmount + self.fee
+            print(
+                f"\nYou have withdrawn {withdrawAmount} and have been charged a ${self.fee:.2f}"
+            )
+            print("Withdraw complete")
+            self.getBalance()
+        except BalanceException as error:
+            print(f"\nWithdraw interupted: {error}")
--- TutorialForBeginners/ch20/oop.py
+++ TutorialForBeginners/ch20/oop.py
@@ -3,7 +3,6 @@ from bank_acounts import *
 
 Bob = BankAccount(100000, "Bob")
 Sam = BankAccount(1000, "Sam")
-
 # run the file to generate the initial creation/deposit messages
 
 Bob.getBalance()
@@ -19,3 +18,19 @@ Bob.withdrawAmount(244)
 
 Bob.transferMoney(99999999999999999, Sam)
 Bob.transferMoney(12345, Sam)
+
+
+Joe = IntrestRewards(10000, "Joe")
+Joe.getBalance()
+# this will add an extra 5%
+Joe.deposit(1000)
+
+# pass in the variable Sam, not the name "Sam"
+Joe.transferMoney(4663, Sam)
+
+
+Ben = SavingsAccount(10000, "Ben")
+
+
+Ben.deposit(1000)
+Ben.withdrawAmount(100)
