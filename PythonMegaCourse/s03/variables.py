056523b6c21dc221067e964ad7c6e5d290f2c0e5
Justin Bailey <sgm13555@gmail.com>
2024-06-17 20:26:34 -0500

    Mega course on Udemy

---
+++ PythonMegaCourse/s03/variables.py
@@ -0,0 +1,13 @@
+spent = 3
+donated = 4
+
+total_amount = spent + donated
+print(total_amount)
+
+
+items = 2
+price = 10
+
+total_price = items * price
+
+print(f"Total price is ${total_price:.2f}")
