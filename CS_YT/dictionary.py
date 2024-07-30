ec6d4230383eb3e57f91d79911ea8c8334eebc9e
Seth Martin <sgm13555@gmail.com>
2024-07-30 16:47:36 -0500

    conditionals

--- CS_YT/dictionary.py
+++ CS_YT/dictionary.py
@@ -16,3 +16,25 @@ print(student.get("phone", "Not Found"))
 # this will update the value
 student["phone"] = "913-333-9999"
 print(student.get("phone", "Not Found"))
+# to update multiple items at once, uses a dictionary as an argument
+student.update({"name": "Bill", "phone": "785-256-8956"})
+print(student)
+
+# to delete a value
+del student["phone"]
+print(student)
+# can use pop to remove a value and store it in a variable
+age = student.pop("age")
+print(age)
+print(student)
+
+student["age"] = 37
+
+# returns the lenght of the dict
+print(len(student))
+# can use key(), values(), items() to get different output
+for value in student.values():
+    print(value)
+
+for key, value in student.items():
+    print(f"{key}: {value}")
