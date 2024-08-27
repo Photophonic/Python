1aec3aefb904b9f064f27de17b4ff7806fe66c9c
Seth Martin <sgm13555@gmail.com>
2024-08-27 16:28:22 -0500

    Update list app

--- PMC24/app1/main.py
+++ PMC24/app1/main.py
@@ -7,22 +7,45 @@
 #     country = input("Enter a country: ")
 #     countries.append(country)
 #     print(countries)
-
-# import builtins
+# import builtinsa
 
 # print(dir(builtins))
 
 
+# todo = []
+# user_val = ""
+
+# while True:
+#     user_val = input("Enter a To Do: \n")
+
+#     if user_val.upper() == "Q":
+#         break
+
+#     todo.append(user_val.title())
+
+# for item in todo:
+#     print(item)
+
 todo = []
-user_val = ""
+user_action = ""
 
 while True:
-    user_val = input("Enter a To Do: \n")
+    user_action = input("Type add, show, edit, or quit: \n")
 
-    if user_val.upper() == "Q":
-        break
+    match user_action.strip():
+        case "add":
+            user_val = input("Enter a To Do: ")
+            todo.append(user_val.title())
+        case "show":
+            for item in todo:
+                print(item)
+        case "edit":
+            edit_item = int(input("Number of item to edit? "))
+            adj_index = edit_item - 1
+            new_todo = input("Enter a new To Do: ")
+            todo[adj_index] = new_todo
 
-    todo.append(user_val.title())
-
-for item in todo:
-    print(item)
+        case "quit":
+            break
+        case _:
+            pass
