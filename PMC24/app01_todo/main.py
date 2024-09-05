b023b2e22ae9504151936dd95e7682cdc891aa8f
Seth Martin <sgm13555@gmail.com>
2024-09-05 16:21:39 -0500

    enumerate

--- PMC24/app01_todo/main.py
+++ PMC24/app01_todo/main.py
@@ -1,35 +1,37 @@
+# empyt to-do list
 todo = []
-user_val = ""
-
-while True:
-    user_val = input("Enter a To Do: \n")
-
-    if user_val.upper() == "Q":
-        break
-
-    todo.append(user_val.title())
-
-for item in todo:
-    print(item)
-
-todo = []
+# initialize varaible for input
 user_action = ""
 
 while True:
-    user_action = input("Type add, show, edit, or quit: \n")
+    user_action = input("Type add, show, edit, complete, or quit: \n")
 
+    # python case statement to compare user_action input
     match user_action.strip():
         case "add":
             user_val = input("Enter a To Do: ")
+            # add to the list the capitalize user_input
             todo.append(user_val.title())
         case "show":
-            for item in todo:
-                print(item)
+            # add enumerate to produce a list and their index
+            for index, item in enumerate(todo):
+                # this creates two variables that can be printed
+                print(f"{index+1}-{item}")
         case "edit":
+            # get the index item from the user
             edit_item = int(input("Number of item to edit? "))
+            # adjust for 0 index
             adj_index = edit_item - 1
+            # create a revised version of the to-do
             new_todo = input("Enter a new To Do: ")
+            # update the value based on the index value
             todo[adj_index] = new_todo
+        case "complete":
+            # pop removes using the index value
+            complete_val = int(input("Number To Do to complete: "))
+            # pop returns the item that was removed from the list
+            removed_val = todo.pop(complete_val - 1)
+            print(f"{removed_val} has been removed")
 
         case "quit":
             break
