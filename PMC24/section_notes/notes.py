be576b4055353cdd5e18c10a148ca3d5cd813a93
Justin Bailey <sgm13555@gmail.com>
2024-10-30 16:25:56 -0500

    decouple

--- PMC24/section_notes/notes.py
+++ PMC24/section_notes/notes.py
@@ -457,3 +457,28 @@ def get_max():
 
 
 print(get_max())
+
+
+# Day 12 decouple functions
+def convert(feet_inches):
+    # returns a list from a string split at the character
+    imperial = feet_inches.split(" ")
+    # assign each list position to a variable
+    feet = float(imperial[0])
+    inches = float(imperial[1])
+
+    meters = feet * 0.3048 + inches * 0.0254
+
+    return meters
+
+
+feet_inches = input("Enter the feet and incehs: ")
+
+meters = convert(feet_inches)
+
+print(f"{meters} meters")
+
+if meters < 1.0:
+    print("too short")
+else:
+    print("have fun")
--- PMC24/section_notes/run_code.py
+++ PMC24/section_notes/run_code.py
@@ -1,6 +1,29 @@
-def get_average():
-    print("Hi")
-    x = "hello"
+def get_feet_inches(feet_inches):
+    imperial = feet_inches.split(" ")
+
+    # assign each list position to a variable
+    feet = float(imperial[0])
+    inches = float(imperial[1])
+
+    return (feet, inches)
 
 
-print(get_average())
+def convert(feet, inches):
+
+    meters = feet * 0.3048 + inches * 0.0254
+
+    return meters
+
+
+feet_inches = input("Enter the feet and incehs: ")
+
+feet, inches = get_feet_inches(feet_inches)
+
+meters = convert(feet, inches)
+
+print(f"{meters} meters")
+
+if meters < 1.0:
+    print("too short")
+else:
+    print("have fun")
