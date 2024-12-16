d424cce1c0a556b59e7fbd3f8cc8500af24dce3c
Justin Bailey <sgm13555@gmail.com>
2024-12-16 16:27:59 -0600

    Zip working

--- PMC24/zipapp/create_zip.py
+++ PMC24/zipapp/create_zip.py
@@ -4,8 +4,10 @@ import pathlib
 
 # the function will iterate over the filepaths and put them into to destination folder.
 def create_archive(filepaths, dest_dir):
+    # create destination path that can concat the text
+    dest_path = pathlib.Path(dest_dir, "archive.zip")
     # similar to with open()
-    with zipfile.ZipFile(dest_dir + "/" + "test.zip", "w") as archive:
+    with zipfile.ZipFile(dest_path, "w") as archive:
         # loop through each path in the filepaths variable
         for filepath in filepaths:
             # write to the archive the files
@@ -15,6 +17,6 @@ def create_archive(filepaths, dest_dir):
 if __name__ == "__main__":
 
     create_archive(
-        filepaths=["zipapp/test.txt", "zipapp/test2.txt", "zipappp/test3.txt"],
-        dest_dir=["zipapp/dest"],
+        filepaths=["zipapp/test.txt", "zipapp/test2.txt", "zipapp/test3.txt"],
+        dest_dir="zipapp/dest",
     )
