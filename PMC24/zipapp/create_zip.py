b71231051b239b3e82d2ccec8608cfc5da9301d8
Justin Bailey <sgm13555@gmail.com>
2024-12-17 16:19:59 -0600

    Complete zip app

--- PMC24/zipapp/create_zip.py
+++ PMC24/zipapp/create_zip.py
@@ -4,19 +4,23 @@ import pathlib
 
 # the function will iterate over the filepaths and put them into to destination folder.
 def create_archive(filepaths, dest_dir):
-    # create destination path that can concat the text
+    # create destination path that can concat the path text
     dest_path = pathlib.Path(dest_dir, "archive.zip")
     # similar to with open()
     with zipfile.ZipFile(dest_path, "w") as archive:
+
         # loop through each path in the filepaths variable
         for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
             # write to the archive the files
-            archive.write(filepath)
+            archive.write(filepath, arcname=filepath.name)
 
 
 if __name__ == "__main__":
 
     create_archive(
+        # list of files & paths
         filepaths=["zipapp/test.txt", "zipapp/test2.txt", "zipapp/test3.txt"],
+        # string dest
         dest_dir="zipapp/dest",
     )
--- PMC24/zipapp/main.py
+++ PMC24/zipapp/main.py
@@ -1,8 +1,7 @@
 # Import GUI
 import FreeSimpleGUI as sg
+from create_zip import create_archive
 
-# create a zip file
-import shutil
 
 # create a series of new elements for the GUI
 label1 = sg.Text("Select files to zip")
@@ -19,6 +18,8 @@ add_button2 = sg.FolderBrowse("Choose", key="destination_btn")
 
 # create button to zip files
 zip_button = sg.Button("Zip Files")
+# create success message
+output_msg = sg.Text(key="output")
 
 # create a window instance,
 # items in layout need to be in brackets. Each pair is a row
@@ -27,7 +28,7 @@ window = sg.Window(
     layout=[
         [label1, input_box1, add_button1],
         [label2, input_box2, add_button2],
-        [zip_button],
+        [zip_button, output_msg],
     ],
 )
 
@@ -35,17 +36,21 @@ window = sg.Window(
 while True:
     # display the list of objects from the window
     event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        # exit loop and close app
+        break
+
     print("Event - ", event)
     print("Values - ", values)
     # get the file patch from the K/V pair above. Will need to split the string with split
     filepath = values["select_btn"].split(";")
     # get the folder path from the destination button dest_btn
     folder = values["destination_btn"]
-    print("filepath - ", filepath)
-    print("folder - ", folder)
+    # pass the file paths into the external function
+    create_archive(filepath, folder)
+    # update success message
+    window["output"].update(value="Zip file completed")
+
 # close the application when we close the window
 window.close()
-
-
-# provide name of path & file name, type, location
-shutil.make_archive("section_notes/myfiles/output", "zip", "section_notes/myfiles")
